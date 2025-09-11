09bcce9a7be14381188130cd73b4e0099fde68f4
yun9017 <yun9017ie@naver.com>
2025-09-11 16:21:20 +0900

    02

--- programming/main.c
+++ programming/main.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    printf("Hello, Sookmyung Electronics!\n");
     return 0;
 }
